78931e0d040455f12819f327751984ebf5ea8f9d
olmur <olexii.muraviov@gmail.com>
2017-09-10 07:52:30 +0300

    add rvt utils and constants

---
+++ rvtools/src/main/java/com/olmur/rvtools/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.olmur.rvtools.utils;
+
+
+public class Constants {
+
+    public static final class Empty {
+        public static final int INT = -1000;
+        public static final long LONG = -1000L;
+        public static final double DOUBLE = -1000.0;
+        public static final float FLOAT = -1000.0F;
+        public static final String STRING = null;
+    }
+}
---
+++ rvtools/src/main/java/com/olmur/rvtools/utils/RvtUtils.java
@@ -0,0 +1,65 @@
+package com.olmur.rvtools.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.Px;
+import android.support.v4.content.ContextCompat;
+import android.util.DisplayMetrics;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RvtUtils {
+
+    public static class Gestures {
+
+        public static void move(List<?> source, int fromPos, int toPos) {
+            if (fromPos < toPos) {
+                for (int i = fromPos; i < toPos; i++) {
+                    Collections.swap(source, i, i + 1);
+                }
+            } else {
+                for (int i = fromPos; i > toPos; i--) {
+                    Collections.swap(source, i, i - 1);
+                }
+            }
+        }
+    }
+
+    public static class Resources {
+
+
+        public static Bitmap getBitmap(Context context, @DrawableRes int drawableRes, @Px int widthPx, @Px int heightPx) {
+            Drawable drawable = ContextCompat.getDrawable(context, drawableRes);
+
+            if (drawable instanceof BitmapDrawable) {
+                return ((BitmapDrawable) drawable).getBitmap();
+            }
+
+            Bitmap bitmap = Bitmap.createBitmap(widthPx, heightPx, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(bitmap);
+            drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+            drawable.draw(canvas);
+            return bitmap;
+        }
+
+        public static int dpToPixels(Context context, int dp) {
+            android.content.res.Resources resources = context.getResources();
+            DisplayMetrics metrics = resources.getDisplayMetrics();
+            return dp * (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        }
+
+
+        public static int pixelsToDp(Context context, int px) {
+            android.content.res.Resources resources = context.getResources();
+            DisplayMetrics metrics = resources.getDisplayMetrics();
+            return px / (metrics.densityDpi / DisplayMetrics.DENSITY_DEFAULT);
+        }
+
+    }
+
+}
